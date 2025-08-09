ab86c8c909a11408070a40e740292dc8dbf743c1
Dmytro_Gunko <dimagunko99@gmail.com>
2025-08-09 16:58:22 +0200

    add toggleMenu

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -2,25 +2,36 @@
 import css from '@/components/TagsMenu/TagsMenu.module.css';
 import { NoteTag } from '@/types/note';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const TagsMenu = () => {
   const tags: NoteTag[] = ['Work', 'Personal', 'Meeting', 'Shopping', 'Todo'];
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
   return (
     <div className={css.menuContainer}>
-      <button className={css.menuButton}>Notes ▾</button>
-      <ul className={css.menuList}>
-        <Link href={`/notes/filter`} className={css.menuLink}>
-          All notes
-        </Link>
-
-        {tags.map((tag) => (
-          <li key={tag} className={css.menuItem}>
-            <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
-              {tag}
+      <button onClick={toggleMenu} className={css.menuButton}>
+        Notes {isOpen ? '▴' : '▾'}
+      </button>
+      {isOpen && (
+        <ul className={css.menuList}>
+          <li className={css.menuItem}>
+            <Link href={`/notes/filter`} className={css.menuLink}>
+              All notes
             </Link>
           </li>
-        ))}
-      </ul>
+
+          {tags.map((tag) => (
+            <li key={tag} className={css.menuItem}>
+              <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
+                {tag}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
